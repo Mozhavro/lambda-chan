e1132d2c7e3a824c409891c01869a81a3d36c8b5
Mozhavro <mozhario@gmail.com>
2019-06-26 16:14:01 +0300

    add web server (ring)

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/tools.nrepl "0.2.13"]]
+                 [org.clojure/tools.nrepl "0.2.13"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-jetty-adapter "1.6.3"]]
   :main ^:skip-aot imbo-chan.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
